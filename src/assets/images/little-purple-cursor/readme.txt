bca16f377574a431410ea6e82e184194c355d72a
crystal / ue <mfihy1@gmail.com>
2023-01-24 14:50:07 -0500

    Update about page, add custom cursors

---
+++ src/assets/images/little-purple-cursor/readme.txt
@@ -0,0 +1,68 @@
+﻿=== Little Purple Neon Glass Cursor Set ===
+
+By: ツ Mimi Destino ♡ (http://www.rw-designer.com/user/38152)
+
+Download: http://www.rw-designer.com/cursor-set/litpurpleneonglassmd
+
+Author's description:
+
+ ツ MIMI DESTINO ♥.♡ 
+
+Enjoy the set.
+Thanks for download and review ✫✫✫✫✫
+
+ 
+
+I try put them on a little size because j request them on the discord channel :)
+
+ 
+
+Purple Big
+http://www.rw-designer.com/cursor-set/purpleneonglassmd
+
+On pink
+http://www.rw-designer.com/cursor-set/pinkneonglassmd
+
+on blue
+http://www.rw-designer.com/cursor-set/blueneonglassmd
+
+Other colors for normal select
+http://www.rw-designer.com/cursor-set/neonglassnsmd
+
+--
+
+How to install? 
+https://youtu.be/VOr3HZHS4fQ
+
+Como instalarlos?
+https://youtu.be/EGx6plXI0Yc
+
+Have any cursor requests? feel free to comment on my profile!
+or here http://www.rw-designer.com/entry/3308
+
+social networks --> http://www.rw-designer.com/entry/3309
+
+my favorite cursors set --> http://www.rw-designer.com/cursor-set/donutskawaiimd
+
+Discord: https://discord.gg/RkvM9FMh
+
+ Credits: I really don't know the real owner because i see them on some pages ... http://www.downcc.com/soft/215418.html
+
+
+ ..\'\'..\'\'..
+ 
+ 
+
+
+==========
+
+License: Creative Commons - Noncommercial
+
+You are free:
+
+* To Share - To copy, distribute and transmit the work.
+* To Remix - To adapt the work.
+
+Under the following conditions:
+
+* Noncommercial - You may not use this work for commercial purposes.
--- src/components/Website/index.js
+++ src/components/Website/index.js
@@ -1,6 +1,10 @@
 import NavBar from '../NavBar';
 import './index.scss';
 import { Routes, Route } from 'react-router-dom';
+import htmlIcon from '../../assets/images/html-icon.png';
+import cssIcon from '../../assets/images/css-icon.png';
+import jsIcon from '../../assets/images/js-icon.png';
+import reactIcon from '../../assets/images/react-icon.png';
 
 const Website = () => {
     return (
@@ -10,7 +14,42 @@ const Website = () => {
                 <Routes>
                     <Route path="/" element={
                         <div className='website-content'>
-                            <text className='headerz'>welcome</text>
+                            <div className="title">
+                                {/* <text className="header">-</text> */}
+                                <text className="header">a</text>
+                                <text className="header">b</text>
+                                <text className="header">o</text>
+                                <text className="header">u</text>
+                                <text className="header">t</text>
+                                <text className="header">!</text>
+                                {/* <text className="header">-</text> */}
+                            </div>
+                            <text>i am a third-year computer science co-op student @ the University of Toronto, passionate about software development, game development, and continous learning.</text>
+                            <br/><br/>
+                            <text>the purpose of this website is to allow me to share my projects, hobbies, and interests, while practicing my web development skills by building a responsive, interactive, fun webpage!</text>
+                            <br/><br/>
+                            <div className="title">
+                                {/* <text className="header">-</text> */}
+                                <text className="header-tech">m</text>
+                                <text className="header-tech">a</text>
+                                <text className="header-tech">d</text>
+                                <text className="header-tech">e</text>
+                                <text className="header-tech">-</text>
+                                <text className="header-tech">w</text>
+                                <text className="header-tech">i</text>
+                                <text className="header-tech">t</text>
+                                <text className="header-tech">h</text>
+                                <text className="header-tech">:</text>
+                                {/* <text className="header">-</text> */}
+                            </div>
+                            {/* <text>ReactJS, JavaScript, HTML, CSS</text> */}
+                            <div className="tech-row">
+                                <a target="_blank" href="https://reactjs.org/"><img className="tech-icon" src={reactIcon}/></a>
+                                <img className="tech-icon" src={jsIcon}/>
+                                <img className="tech-icon" src={htmlIcon}/>
+                                <img className="tech-icon" src={cssIcon}/>
+                            </div>
+                            {/* <text className='header'>-welcome-</text> */}
                         </div>
                     }/>
                 </Routes>
