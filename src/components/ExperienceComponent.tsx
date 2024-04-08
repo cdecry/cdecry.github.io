f2f911a8ab1473a2d9f041077ba7edc1b4b7eadc
crystal / ue <mfihy1@gmail.com>
2024-04-08 14:01:10 -0400

    Add work experience timeline

--- src/components/ExperienceComponent.tsx
+++ src/components/ExperienceComponent.tsx
@@ -1,4 +1,4 @@
-import { Text } from "@mantine/core";
+import { Text, Space, Timeline } from "@mantine/core";
 
 const ExperienceComponent = () => {
 
@@ -6,9 +6,39 @@ const ExperienceComponent = () => {
         <div className='tab'>
             <div className='header'></div>
             <div className='content'>
-                <Text>
-                    experience
+                <Text size="xl" fw={800}>
+                    &#9135; work experience
                 </Text>
+                <Space h="md" />
+                <Timeline active={2} bulletSize={24} lineWidth={2}>
+                    <Timeline.Item 
+                        title="HubSpot"
+                    >
+                        <Text c="dimmed" size="sm">Incoming Software Engineer Intern</Text>
+                        <Text size="xs" mt={4}>September 2024 - December 2024</Text>
+                    </Timeline.Item>
+
+                    <Timeline.Item 
+                        title="Dayforce"
+                    >
+                        <Text c="dimmed" size="sm">Software Engineer Intern</Text>
+                        <Text size="xs" mt={4}>January 2024 - August 2024</Text>
+                    </Timeline.Item>
+
+                    <Timeline.Item 
+                        title="KHERG"
+                    >
+                        <Text c="dimmed" size="sm">Software Engineer</Text>
+                        <Text size="xs" mt={4}>September 2023 - August 2024</Text>
+                    </Timeline.Item>
+
+                    <Timeline.Item 
+                        title="University Health Network"
+                    >
+                        <Text c="dimmed" size="sm">Software Engineer Intern</Text>
+                        <Text size="xs" mt={4}>May 2023 - August 2023</Text>
+                    </Timeline.Item>
+                    </Timeline>
             </div>
         </div>
     );
