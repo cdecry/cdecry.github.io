4f87960904527c525a19511e27ad1b8e2c736995
crystal / ue <mfihy1@gmail.com>
2024-04-08 17:57:46 -0400

    Add horizontal section and made with grid

--- src/components/AboutComponent.tsx
+++ src/components/AboutComponent.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Text, ThemeIcon, Timeline } from "@mantine/core";
+import { Avatar, Card, SimpleGrid, Text, ThemeIcon, Timeline } from "@mantine/core";
 import { IconSun, IconVideo, IconHtml } from '@tabler/icons-react';
 
 const AboutComponent = () => {
@@ -7,22 +7,32 @@ const AboutComponent = () => {
         <div className='tab'>
             {/* <div className='header'></div> */}
             <div className='content'>
-                <Text size="xl" fw={800}>
-                    &#9135; welcome
-                </Text>
-                <Text>
-                    hi! my name is <em id='crystal'>crystal</em>, and i'm a 
-                    third-year computer science co-op student @ the University of 
-                    Toronto, passionate about software development and its innovative 
-                    (and fun) applications.<br></br><br></br>
+                <div className='horizontal-section'>
+                    <div>
+                        <Text size="xl" fw={800}>
+                            &#9135; welcome
+                        </Text>
+                        <Text>
+                            hi! my name is <em id='crystal'>crystal</em>, and i'm a 
+                            third-year computer science co-op student @ the University of 
+                            Toronto, passionate about software development and its innovative 
+                            (and fun) applications. i'm always learning and exploring new 
+                            technologies!<br></br><br></br>
 
-                    the purpose of this website is to allow me to share my projects, 
-                    hobbies, and interests, while practicing my web development skills 
-                    by building a responsive, interactive, fun experience.<br></br><br></br>
-                    
-                    fun fact: this is actually the 4th iteration of my personal 
-                    website!<br></br><br></br>
-                </Text>
+                            the purpose of this website is to allow me to share my projects, 
+                            hobbies, and interests, while practicing my web development skills 
+                            by building a responsive, interactive, fun experience.
+                        </Text>
+                    </div>
+                    <Card className='made-with-container'>
+                    <SimpleGrid cols={2} verticalSpacing="sm">
+                        <div>React</div>
+                        <div>HTML5</div>
+                        <div>CSS</div>
+                        <div>TypeScript</div>
+                    </SimpleGrid>
+                    </Card>
+                </div>
             </div>
         </div>
     );
