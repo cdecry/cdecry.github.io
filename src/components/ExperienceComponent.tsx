4fd6aa2f6e0bdf428964979c190274f1ae80ecf8
crystal / ue <mfihy1@gmail.com>
2024-04-08 14:40:54 -0400

    Add company icons outline and shadow

--- src/components/ExperienceComponent.tsx
+++ src/components/ExperienceComponent.tsx
@@ -1,5 +1,8 @@
 import { Text, Space, Timeline, Avatar } from "@mantine/core";
-const hubspotLogo = require('../images/hubspot-logo.png');
+const hubspotIcon = require('../images/hubspot-icon.png');
+const dayforceIcon = require('../images/dayforce-icon.png');
+const khergIcon = require('../images/kherg-icon.png');
+const uhnIcon = require('../images/uhn-icon.png');
 
 const ExperienceComponent = () => {
 
@@ -16,7 +19,7 @@ const ExperienceComponent = () => {
                     <Timeline.Item 
                         title="HubSpot"
                         bullet={
-                            <Avatar src={hubspotLogo} />
+                            <Avatar className="company-icon" src={hubspotIcon} />
                         }
                     >
                         <Text c="dimmed" size="sm">Incoming Software Engineer Intern</Text>
@@ -25,6 +28,9 @@ const ExperienceComponent = () => {
 
                     <Timeline.Item 
                         title="Dayforce"
+                        bullet={
+                            <Avatar className="company-icon" src={dayforceIcon} />
+                        }
                     >
                         <Text c="dimmed" size="sm">Software Engineer Intern</Text>
                         <Text size="xs" mt={4}>January 2024 - August 2024</Text>
@@ -32,6 +38,9 @@ const ExperienceComponent = () => {
 
                     <Timeline.Item 
                         title="Kidney Health Education and Research Group"
+                        bullet={
+                            <Avatar className="company-icon" src={khergIcon} />
+                        }
                     >
                         <Text c="dimmed" size="sm">Software Engineer</Text>
                         <Text size="xs" mt={4}>September 2023 - August 2024</Text>
@@ -39,6 +48,9 @@ const ExperienceComponent = () => {
 
                     <Timeline.Item 
                         title="University Health Network"
+                        bullet={
+                            <Avatar className="company-icon" src={uhnIcon} />
+                        }
                     >
                         <Text c="dimmed" size="sm">Software Engineer Intern</Text>
                         <Text size="xs" mt={4}>May 2023 - August 2023</Text>
