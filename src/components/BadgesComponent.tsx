22856e94c078dfd5db9bc1c593c67d57d3e52c83
crystal / ue <mfihy1@gmail.com>
2024-04-09 10:19:00 -0400

    Fix badge display with flex

--- src/components/BadgesComponent.tsx
+++ src/components/BadgesComponent.tsx
@@ -1,15 +1,24 @@
-"use client";
-
 const BadgesComponent = () => {
   return (
     <div className="badge-container">
         <span className='badge blue'>C#</span>
         <span className='badge gray'>Java</span>
         <span className='badge red'>Python</span>
-        <span className='badge purple'>JavaScript</span>
-        <span className='badge green'>TypeScript</span>
+        <span className='badge green'>JavaScript</span>
+        <span className='badge yellow'>TypeScript</span>
         <span className='badge indigo'>HTML</span>
-        <span className='badge pink'>CSS</span>
+        <span className='badge purple'>CSS</span>
+
+        {/* <libraries />
+        <frameworks></frameworks> */}
+        <span className='badge pink'>React.js</span>
+        <span className='badge pink'>Node.js</span>
+        <span className='badge pink'>TailwindCSS</span>
+
+
+        <span className='badge pink'>PostgreSQL</span>
+        <span className='badge pink'>MySQL</span>
+        <span className='badge pink'>MongoDB</span>
 
     </div>
   );
