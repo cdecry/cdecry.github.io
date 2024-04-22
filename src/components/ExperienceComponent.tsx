12cc9948b44a8b7f9f3f30f06dfcbdcdc76dfc6c
crystal / ue <mfihy1@gmail.com>
2024-04-22 15:52:11 -0400

    Update experience page timelin

--- src/components/ExperienceComponent.tsx
+++ src/components/ExperienceComponent.tsx
@@ -4,6 +4,7 @@ const hubspotIcon = require('../images/hubspot-icon.png');
 const dayforceIcon = require('../images/dayforce-icon.png');
 const khergIcon = require('../images/kherg-icon.png');
 const uhnIcon = require('../images/uhn-icon.png');
+const uoftIcon = require('../images/uoft-icon.png');
 const mmhsIcon = require('../images/mmhs-icon.png');
 
 const ExperienceComponent = () => {
@@ -35,56 +36,68 @@ const ExperienceComponent = () => {
                 <Timeline color="#a98cedff" reverseActive active={2} bulletSize={24} lineWidth={6}>
                     <Timeline.Item
                         className="fade-in-node"
-                        title="HubSpot"
+                        title="Software Engineer Intern"
                         bullet={
                             <Avatar className="company-icon" src={hubspotIcon} />
                         }
                     >
-                        <Text size="sm">Incoming Software Engineer Intern</Text>
+                        <Text size="sm">HubSpot</Text>
                         <Text size="xs" mt={4}>September 2024 - December 2024</Text>
                     </Timeline.Item>
 
                     <Timeline.Item 
                         className="fade-in-node"
-                        title="Dayforce"
+                        title="Software Engineer Intern"
                         bullet={
                             <Avatar className="company-icon" src={dayforceIcon} />
                         }
                     >
-                        <Text size="sm">Software Engineer Intern</Text>
+                        <Text size="sm">Dayforce</Text>
                         <Text size="xs" mt={4}>May 2024 - August 2024</Text>
                     </Timeline.Item>
 
                     <Timeline.Item 
                         className="fade-in-node"
-                        title="Dayforce"
+                        title="Machine Learning Researcher"
+                        bullet={
+                            <Avatar className="company-icon" src={uoftIcon} />
+                        }
+                    >
+                        <Text size="sm">University of Toronto</Text>
+                        <Text size="xs" mt={4}>May 2024 - August 2024</Text>
+                    </Timeline.Item>
+
+
+                    <Timeline.Item 
+                        className="fade-in-node"
+                        title="Software Engineer Intern"
                         bullet={
                             <Avatar className="company-icon" src={dayforceIcon} />
                         }
                     >
-                        <Text size="sm">Software Engineer Intern</Text>
+                        <Text size="sm">Dayforce</Text>
                         <Text size="xs" mt={4}>January 2024 - April 2024</Text>
                     </Timeline.Item>
 
                     <Timeline.Item 
                         className="fade-in-node"
-                        title="Kidney Health Education and Research Group"
+                        title="Software Engineer"
                         bullet={
                             <Avatar className="company-icon" src={khergIcon} />
                         }
                     >
-                        <Text size="sm">Software Engineer</Text>
+                        <Text size="sm">Kidney Health Education and Research Group</Text>
                         <Text size="xs" mt={4}>September 2023 - August 2024</Text>
                     </Timeline.Item>
 
                     <Timeline.Item 
                         className="fade-in-node"
-                        title="University Health Network"
+                        title="Software Engineer Intern"
                         bullet={
                             <Avatar className="company-icon" src={uhnIcon} />
                         }
                     >
-                        <Text size="sm">Software Engineer Intern</Text>
+                        <Text size="sm">University Health Network</Text>
                         <Text size="xs" mt={4}>May 2023 - August 2023</Text>
                     </Timeline.Item>
 
